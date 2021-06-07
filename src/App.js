f76a97515eec47a84cc2579b24cbc9ddfefcb489
Joshua Vaughn <JKVon12345@gmail.com>
2021-06-07 08:01:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useReducer} from 'react';
+import CoursesList from './CoursesList'
+import Search from './Search';
 
-function App() {
+
+ const courses_data = [
+   {
+     id: 1, 
+     title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+     author: "Maximilian Schwarzmuller",
+     hours_video: 40.5,
+     number_of_lectures: 490,
+     rating: 4.6,
+     url: "https://codingthesmartway.com/courses/react-complete-guide/"
+   },
+   {
+    id: 2, 
+    title: "Modern React with redux",
+    author: "Stephen Grider",
+    hours_video: 47.5,
+    number_of_lectures: 488,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/modern-react-with-redux/"
+   },
+   {
+    id: 3, 
+    title: "The Complete React Developer Course (w/ Hooks and Redux)",
+    author: "Andrew Mead",
+    hours_video: 39,
+    number_of_lectures: 200,
+    rating: 4.7,
+    url: "https://codingthesmartway.com/courses/complete-react-web-app-development/"
+   }
+ ];
+
+// const coursesReducer = (state, action) => {
+//   switch(action.type) {
+//     case 'SET_COURSES':
+//       return action.payload;
+//     case 'REMOVE_COURSES':
+//       return state.filter(
+//         course => action.payload.id !== course.id
+//       )
+//     default:
+//       throw new Error();
+//   }
+// };
+
+const coursesReducer = (state, action) => {
+    switch(action.type) {
+      case 'FETCH_COURSES_START':
+        return {
+          ...state,
+          isLoading: true,
+
+        };
+      case 'FETCH_COURSES_SUCCESS':
+        return {
+          ...state,
+          isLoading: false,
+          data: action.payload
+
+        };
+      case 'REMOVE_COURSE':
+        return {
+          ...state,
+          data: state.filter(course => action.payload.id !== course.id)
+        };
+      default:
+        throw new Error();
+    }
+}
+
+const App = () => {
+
+
+  const STRAPI_API_ENDPOINT = 'http://localhost:1337/courses'
+
+  const [courses, dispatchCourses] = useReducer(
+    coursesReducer,
+    {data: [], isLoading: false}
+  );
+
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const [searchText, setSearchText] = useState(localStorage.getItem('searchText') || '')
+
+  const handleSearch = event => { 
+    setSearchText(event.target.value);
+  }
+
+  const handleRemoveCourse = course => { 
+    dispatchCourses({
+      type: 'REMOVE_COURSE',
+      payload: course
+    });
+  }
+
+  // const getCoursesAsync = () =>
+  //   new Promise (resolve =>
+  //     setTimeout(
+  //       () => resolve({courses: courses_data}), 2000
+  //     )
+  //   );
+
+  useEffect(() => {
+    dispatchCourses({type: 'FETCH_COURSES_START'});
+    fetch(STRAPI_API_ENDPOINT)
+      .then(
+        response => response.json())
+      .then(result => {
+        dispatchCourses({
+          type: 'FETCH_COURSES_SUCCESS',
+          payload: result})
+      })
+      .catch((e) => console.log("Error fetching courses from back-end..."));
+    }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('searchText', searchText)
+  }, [searchText]);
+
+  const filteredCourses = courses.data.filter(course => {
+    return course.title.includes(searchText) || course.author.includes(searchText)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List of Courses</h1>
+      <hr/>
+      <Search value={searchText} onSearch={handleSearch}/>
+
+      {courses.isLoading ? (
+        <p>Loading courses...</p>
+      ) : (
+        <CoursesList courses={filteredCourses} handleRemoveCourse={handleRemoveCourse}/>
+      )}
     </div>
   );
 }
---
+++ src/CoursesList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const CoursesList = ({courses, handleRemoveCourse}) => {
+    return courses.map(course => {
+      return (
+        <div key={course.id}>
+          <span>
+            <a href={course.url}><h4>{course.title}</h4></a>
+          </span>
+          <span>by <strong>{course.authors.map(author => author.first_name + " " + author.last_name)} </strong></span>
+          <span> | Video Hours: {course.hours_video}</span>
+          <span> | Number of Lectures: {course.number_of_lectures}</span>
+          <span> | Rating: {course.rating}</span>
+          <br/><br/>
+          <span>
+              <button type="button" onClick={() => handleRemoveCourse(course)}>
+                  Remove
+              </button>
+          </span>
+        </div>
+      );
+    })
+}
+
+export default CoursesList;
